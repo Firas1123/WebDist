c90666c09685cd8ddaa82b4d11a1685831c2ac3a
RegaiegMahdi <100922107+RegaiegMahdi@users.noreply.github.com>
2025-04-08 06:33:25 +0100

    Initial commit of Projet microservice

---
+++ src/main/java/com/esprit/ms/projet/Entity/Projet.java
@@ -0,0 +1,28 @@
+package com.esprit.ms.projet.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Projet implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    private Long userId; // placeholder for future User-MS integration
+}
